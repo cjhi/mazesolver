332c10b70825f56ea73fe4347e2fb7e1d5a0f39f
cmot17 <25753364+cmot17@users.noreply.github.com>
2023-12-08 14:19:19 -0500

    Add STL generation and fix large maze

--- maze_gen/image_gen.py
+++ maze_gen/image_gen.py
@@ -1,14 +1,18 @@
+from PIL import Image, ImageDraw
+import trimesh
+import numpy as np
+
+# trimesh needs "pip install "pyglet<2"", "pip install manifold3d"
+
 # Open and read the maze file
-maze_file = open("example_maze.txt", "r")
+maze_file = open("large_example.txt", "r")
 text_maze = maze_file.read()
 
 # Split the maze into lines
 text_lines = text_maze.splitlines()
 
-from PIL import Image, ImageDraw
-
 # Define the size of the maze
-maze_size = (3, 3)
+maze_size = (10, 10)
 
 # Define the number of pixels per unit
 pixels_per_unit = 20
@@ -24,6 +28,8 @@ image = Image.new("RGB", image_size, "white")
 
 # Create a draw object
 draw = ImageDraw.Draw(image)
+
+boxes = []
 
 # Loop through each line of the maze
 for i, line in enumerate(text_lines):
@@ -45,7 +51,23 @@ for i, line in enumerate(text_lines):
                 ),
                 fill="black",
             )
-        # If the character is an empty space, draw a white rectangle
+            bounds = np.array(
+                [
+                    [
+                        x_pos,
+                        (y_pos) - (wall_thickness / 2),
+                        0,
+                    ],
+                    [
+                        (x_pos) + 1,
+                        (y_pos) + (wall_thickness / 2),
+                        1,
+                    ],
+                ]
+            )
+            box = trimesh.creation.box(bounds=bounds)
+            boxes.append(box)
+        # If the character is a vertical wall, draw a black rectangle
         elif char == "|":
             y_pos = i / 2
             x_pos = j / 2
@@ -58,6 +80,28 @@ for i, line in enumerate(text_lines):
                 ),
                 fill="black",
             )
+            bounds = np.array(
+                [
+                    [
+                        x_pos - (wall_thickness / 2),
+                        (y_pos),
+                        0,
+                    ],
+                    [
+                        (x_pos) + (wall_thickness / 2),
+                        (y_pos) + 1,
+                        1,
+                    ],
+                ]
+            )
+            box = trimesh.creation.box(bounds=bounds)
+            boxes.append(box)
+
+# Combine all of the boxes together
+maze_mesh = trimesh.util.concatenate(boxes)
+boolean_maze = trimesh.boolean.union(boxes)
+boolean_maze.apply_scale(1000)
+boolean_maze.export("maze.stl")
 
 # Save the image
 image.save("maze.png")
